4e052d8de61a2924a15891f954fd197eeebf9af0
Alexander Sustavov <markowillz@gmail.com>
2024-08-05 22:27:08 +0600

    Add BasicAuth

---
+++ app/Http/Middleware/BasicAuth.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class BasicAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        header('Cache-Control: no-cache, must-revalidate, max-age=0');
+
+        if ($this->isUnauthorized()) {
+            header('HTTP/1.1 401 Authorization Required');
+            header('WWW-Authenticate: Basic realm="Access denied"');
+
+            exit;
+        }
+
+        return $next($request);
+    }
+
+    private function isUnauthorized(): bool
+    {
+        $AUTH_USER = 'latvia';
+        $AUTH_PASS = 'ponos';
+
+        return empty($_SERVER['PHP_AUTH_USER'])
+            || empty($_SERVER['PHP_AUTH_PW'])
+            || $_SERVER['PHP_AUTH_USER'] != $AUTH_USER
+            || $_SERVER['PHP_AUTH_PW']   != $AUTH_PASS;
+    }
+}

6b6b90ec6f7539b32287a67909a255fd27132fce
Alexander Sustavov <markowillz@gmail.com>
2024-09-12 22:07:08 +0600

    Create UserToken to manage config opening

---
+++ app/Http/Controllers/UserTokenController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserToken;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class UserTokenController extends Controller
+{
+    public function index()
+    {
+        $userTokens = UserToken::query()
+            ->where('expires_at', '>', now())
+            ->get();
+
+        return view('user-tokens.index', compact('userTokens'));
+    }
+
+    public function create()
+    {
+        $users = User::orderBy('name')->get();
+
+        return view('user-tokens.create', compact('users'));
+    }
+
+    public function store(Request $request)
+    {
+        $user = User::find($request->user_id);
+
+        $token = $user->tokens()->create([
+            'token' => Str::random(40),
+            'password' => Str::random(10)
+        ]);
+
+        return redirect()->route('user-tokens.show', $token->id);
+    }
+
+    public function destroy(UserToken $userToken)
+    {
+        $userToken->delete();
+        return redirect()->route('user-tokens.index');
+    }
+
+    public function show(UserToken $userToken)
+    {
+        if ($userToken->expires_at && now()->gte(Carbon::parse($userToken->expires_at))) {
+            abort(404);
+        }
+
+        return view('user-tokens.show', compact('userToken'));
+    }
+}
---
+++ app/Models/UserToken.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class UserToken extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'token',
+        'password',
+        'expires_at',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function validateToken(?string $password): bool
+    {
+        if (now()->gte(Carbon::parse($this->expires_at))) {
+            abort(404);
+        }
+
+        return $password === $this->password;
+    }
+}

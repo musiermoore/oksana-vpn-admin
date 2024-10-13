dcf2f512e5fd9604592997cb05d2b0f5403b2ccd
Alexander Sustavov <markowillz@gmail.com>
2024-10-14 00:26:36 +0600

    Create a service to calculate and parse traffic

---
+++ app/Services/WireGuardTrafficService.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Config;
+use Exception;
+
+class WireGuardTrafficService
+{
+    public const SENT_TYPE = 'sent';
+    public const RECEIVED_TYPE = 'received';
+    public const ALLOWED_TYPES = [
+        self::SENT_TYPE,
+        self::RECEIVED_TYPE,
+    ];
+
+    private Config $config;
+    private string $transfer;
+    private array $traffic;
+
+    public function __construct(Config $config, $transfer)
+    {
+        $this->config = $config;
+        $this->transfer = $transfer;
+    }
+
+    public function calculate()
+    {
+        try {
+            $this->parseData();
+        } catch (Exception $exception) {
+            return [];
+        }
+
+        return $this->traffic;
+    }
+
+    private function parseData()
+    {
+        $transferTypes = explode(', ', $this->transfer);
+
+        foreach ($transferTypes as $type) {
+            try {
+                preg_match('/(\d{0,9}\.?\d{1,9}?) ([TGM]iB) (received|sent)/', $type, $matches);
+
+                [$string, $amount, $unit, $type] = $matches;
+
+                if (empty($type) || !in_array($type, self::ALLOWED_TYPES)) {
+                    continue;
+                }
+
+                $this->traffic[$type] = $this->calculateAmount($amount, $unit);
+            } catch (Exception $exception) {
+                continue;
+            }
+        }
+
+        $this->traffic['config_id'] = $this->config->id;
+        $this->traffic['created_at'] = now();
+        $this->traffic['updated_at'] = now();
+    }
+
+
+    /**
+     * Convert to bytes
+     *
+     * @param $amount
+     * @param $unit
+     * @return int
+     */
+    private function calculateAmount($amount, $unit): int
+    {
+        return (int) round($amount * $this->convertUnit($unit));
+    }
+
+    /**
+     * Convert to bytes
+     *
+     * @param $unit
+     * @return float|int
+     */
+    private function convertUnit($unit): float|int
+    {
+        $units = [
+            'TiB' => 1024 * 1024 * 1024 * 1024,
+            'GiB' => 1024 * 1024 * 1024,
+            'MiB' => 1024 * 1024,
+            'KiB' => 1024,
+        ];
+
+        return $units[$unit] ?? 1;
+    }
+}

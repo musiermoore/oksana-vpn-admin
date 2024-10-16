53690853000569ac889cc0b186d62b4b2875ae48
Alexander Sustavov <markowillz@gmail.com>
2024-10-16 23:41:11 +0600

    Fix the relation to get download a config file

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -58,7 +58,7 @@ class UserController extends Controller
             ], 404);
         }
 
-        $config = $user->configs
+        $config = $user->configs()
             ->where('name', $config)
             ->first();
 

49ceb7c9141a101833423fbbd46833b3cb03d28e
Alexander Sustavov <markowillz@gmail.com>
2024-10-16 19:05:26 +0600

    Increase time to run detecting high usage traffic

--- routes/console.php
+++ routes/console.php
@@ -4,4 +4,4 @@ use App\Console\Commands\CalculatePeersTraffic;
 use App\Console\Commands\DetectHighTraffic;
 
 Schedule::command(CalculatePeersTraffic::class)->everyMinute();
-Schedule::command(DetectHighTraffic::class)->everyMinute();
+Schedule::command(DetectHighTraffic::class)->everyThreeMinutes();

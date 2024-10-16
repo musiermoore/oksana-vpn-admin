30eab166d34565ffee4d13d8f959c7d26619f3df
Alexander Sustavov <markowillz@gmail.com>
2024-10-16 21:25:57 +0600

    Disable interactive on creating/deleting configs

--- app/Models/Server.php
+++ app/Models/Server.php
@@ -24,6 +24,6 @@ class Server extends Model
 
     public function getSshCommandAttribute(): string
     {
-        return "ssh root@{$this->ip}";
+        return "ssh -o BatchMode=yes -o StrictHostKeyChecking=no root@{$this->ip}";
     }
 }

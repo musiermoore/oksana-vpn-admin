efd26bb0bc6454d7a73856e936d37fb77049bf7f
Alexander Sustavov <markowillz@gmail.com>
2024-10-18 23:21:11 +0600

    Add a migration to set limits to all configs

---
+++ database/migrations/2024_10_18_171330_set_limits_to_all_configs.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Models\Limit;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        ini_set('memory_limit', '-1');
+        ini_set('max_execution_time', 300);
+
+        $configs = Config::all();
+
+        $limits = [];
+        foreach ($configs as $config) {
+            try {
+                $result = $config->setSpeedLimit(30);
+
+                if ($result === 0) {
+                    $limits[] = [
+                        'config_id' => $config->id,
+                        'amount' => 30
+                    ];
+                }
+            } catch (Exception $exception) {
+
+            }
+        }
+
+        Limit::insert($limits);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        \Illuminate\Support\Facades\DB::table('limits')->delete();
+    }
+};

0c4ae0555141b63f88dd0d8e40e07bbf8815beda
Alexander Sustavov <markowillz@gmail.com>
2024-11-16 16:32:41 +0600

    Add 'after' to is_approved migration to make it after amount

--- database/migrations/2024_11_16_100900_add_is_approved_column_to_transactions_table.php
+++ database/migrations/2024_11_16_100900_add_is_approved_column_to_transactions_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('transactions', function (Blueprint $table) {
-            $table->boolean('is_approved')->default(false);
+            $table->boolean('is_approved')->default(false)->after('amount');
         });
 
         DB::table('transactions')->update(['is_approved' => true]);

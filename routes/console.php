286edf647c93e866ae301642d62da48f8e299e30
Alexander Sustavov <markowillz@gmail.com>
2026-03-15 17:03:28 +0600

    Add a command to pull vless configs to the console kernel

--- routes/console.php
+++ routes/console.php
@@ -4,10 +4,12 @@ use App\Console\Commands\AddExtraPayments;
 use App\Console\Commands\CalculatePeersTraffic;
 use App\Console\Commands\DetectHighTraffic;
 use App\Console\Commands\DisableConfigsOfOverdueDebtorsCommand;
+use App\Console\Commands\PullVlessConfigs;
 use App\Console\Commands\RemoveOldTrafficLogs;
 
 Schedule::command(CalculatePeersTraffic::class)->everyMinute();
 Schedule::command(DetectHighTraffic::class)->everyMinute();
 Schedule::command(RemoveOldTrafficLogs::class)->everyMinute();
+Schedule::command(PullVlessConfigs::class)->everyFiveMinutes();
 Schedule::command(AddExtraPayments::class)->hourly();
 Schedule::command(DisableConfigsOfOverdueDebtorsCommand::class)->everyThirtyMinutes();

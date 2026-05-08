c64a85440ec037b46f6db2042ca8ca2393a83a5c
Alexander Sustavov <markowillz@gmail.com>
2026-05-09 00:47:44 +0600

    Fix booting telegram bot settings

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use GuzzleHttp\Client;
 use Illuminate\Support\ServiceProvider;
+use Telegram\Bot\BotsManager;
 use Telegram\Bot\HttpClients\GuzzleHttpClient;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,25 +14,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
-    }
+        $this->app->extend(BotsManager::class, function (BotsManager $manager, $app): BotsManager {
+            $proxy = (string) config('telegram.proxy', '');
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        $proxy = (string) config('telegram.proxy', '');
+            if ($proxy === '') {
+                return $manager;
+            }
 
-        if ($proxy === '') {
-            return;
-        }
-
-        config()->set(
-            'telegram.http_client_handler',
-            new GuzzleHttpClient(new Client([
+            $config = config('telegram');
+            $config['http_client_handler'] = new GuzzleHttpClient(new Client([
                 'proxy' => $proxy,
-            ])),
-        );
+            ]));
+
+            return (new BotsManager($config))->setContainer($app);
+        });
     }
 }

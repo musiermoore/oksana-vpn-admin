5f20c0995b34e9dfed935a9da4ccb5253da0dd44
Alexander Sustavov <markowillz@gmail.com>
2026-06-15 12:29:14 +0600

    Disable DetectHighTraffic scheduler command

--- routes/console.php
+++ routes/console.php
@@ -3,14 +3,12 @@
 use App\Console\Commands\AddExtraPayments;
 use App\Console\Commands\CalculatePeersTraffic;
 use App\Console\Commands\CreateDefaultConfigsForActiveSubscribersCommand;
-use App\Console\Commands\DetectHighTraffic;
 use App\Console\Commands\DisableConfigsOfOverdueDebtorsCommand;
 use App\Console\Commands\PullVlessConfigs;
 use App\Console\Commands\RenewSubscriptionsCommand;
 use App\Console\Commands\RemoveOldTrafficLogs;
 
 Schedule::command(CalculatePeersTraffic::class)->everyMinute();
-Schedule::command(DetectHighTraffic::class)->everyMinute();
 Schedule::command(RemoveOldTrafficLogs::class)->everyMinute();
 Schedule::command(PullVlessConfigs::class)->everyMinute();
 Schedule::command(AddExtraPayments::class)->hourly();
